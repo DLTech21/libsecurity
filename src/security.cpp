8108f479704aa394b734b0fc312fda9166093767
Donal <donal.tong@gmail.com>
2018-07-11 17:40:28 +0800

    lib

--- src/security.cpp
+++ src/security.cpp
@@ -2,10 +2,6 @@
  *   Copyright (C) 2015 All rights reserved.
  *
  *   文件名称：security.cpp
- *   创 建 者：Zhang Yuanhao
- *   邮    箱：bluefoxah@gmail.com
- *   创建日期：2015年01月29日
- *   描    述：
  *
  #include "security.h"
  ================================================================*/
@@ -24,8 +20,8 @@
 #include "aes.h"
 #include "aes_locl.h"
 #include "base64.h"
+#include "../../include/security.h"
 #include "md5.h"
-#include "security.h"
 
 uint32_t ReadUint32(uchar_t *buf)
 {
@@ -48,7 +44,7 @@ extern "C" {
     
 #ifdef __ANDROID__
 
-    jbyteArray Java_com_dtalk_dd_Security_EncryptMsg(JNIEnv* env, jobject obj, jstring jstr)
+    jbyteArray Java_io_github_dltech21_Security_EncryptMsg(JNIEnv* env, jobject obj, jstring jstr, jstring jstrKey)
     {
         const char *pInData = env->GetStringUTFChars(jstr, NULL);		//待加密内容,转换格式
         uint32_t nInLen = strlen(pInData);
@@ -69,7 +65,7 @@ extern "C" {
         
         WriteUint32((pData + nEncryptLen - 4), nInLen);
         AES_KEY aesKey;
-        const char *key = "00111946655405188575651534545107";
+        const char *key = env->GetStringUTFChars(jstrKey, NULL);
         AES_set_encrypt_key((const unsigned char*)key, 256, &aesKey);
         for (uint32_t i = 0; i < nBlocks; i++) {
             AES_encrypt(pData + i * 16, pEncData + i * 16, &aesKey);
@@ -78,6 +74,7 @@ extern "C" {
         free(pData);
         string strEnc((char*)pEncData, nEncryptLen);
         free(pEncData);
+        env->ReleaseStringUTFChars(jstrKey,key);
         string strDec = base64_encode(strEnc);
         
         jbyteArray carr = env->NewByteArray(strDec.length());
@@ -88,7 +85,7 @@ extern "C" {
     /**
      * 解密
      */
-    jbyteArray Java_com_dtalk_dd_Security_DecryptMsg(JNIEnv* env, jobject obj, jstring jstr)
+    jbyteArray Java_io_github_dltech21_Security_DecryptMsg(JNIEnv* env, jobject obj, jstring jstr, jstring jstrKey)
     {
         const char *pInData = env->GetStringUTFChars(jstr, NULL);   //获取待揭秘内容,转换格式
         uint32_t nInLen = strlen(pInData);
@@ -114,7 +111,7 @@ extern "C" {
         uint32_t nBlocks = nLen / 16;
         AES_KEY aesKey;
         
-        const char *key = "00111946655405188575651534545107";
+        const char *key = env->GetStringUTFChars(jstrKey, NULL);
         AES_set_decrypt_key((const unsigned char*) key, 256, &aesKey);           //设置AES解密密钥
         for (uint32_t i = 0; i < nBlocks; i++) {
             AES_decrypt(pData + i * 16, (unsigned char*)pTmp + i * 16, &aesKey);
@@ -131,11 +128,12 @@ extern "C" {
         pTmp[nOutLen] = 0;
         jbyteArray carr = env->NewByteArray(nOutLen);
         env->SetByteArrayRegion(carr,0,nOutLen,(jbyte*)pTmp);
+        env->ReleaseStringUTFChars(jstrKey,key);
         free(pTmp);
         return carr;
     }
     
-    jbyteArray Java_com_dtalk_dd_Security_EncryptPass(JNIEnv* env, jobject obj, jstring jstr)
+    jbyteArray Java_io_github_dltech21_Security_EncryptPass(JNIEnv* env, jobject obj, jstring jstr)
     {
         const char *pInData = env->GetStringUTFChars(jstr, NULL);		//待加密内容,转换格式
         uint32_t nInLen = strlen(pInData);
@@ -186,7 +184,6 @@ extern "C" {
         
         const char *key = "00111946655405188575651534545107";
         AES_set_encrypt_key((const unsigned char*)key, 256, &aesKey);
-
         for (uint32_t i = 0; i < nBlocks; i++) {
             AES_encrypt(pData + i * 16, pEncData + i * 16, &aesKey);
         }
