b2e15283c2110a855f7766b8c6d151d550a3da4c
Donal <donal.tong@gmail.com>
2018-08-31 15:43:22 +0800

    android 增加校验签名

--- android/jni/security.cpp
+++ android/jni/security.cpp
@@ -43,6 +43,144 @@ extern "C" {
 #endif
     
 #ifdef __ANDROID__
+    
+    //签名信息
+    const char *app_sha1="FD305F186972DEA0F22F09C72C03975F6ACB02DB";
+    const char hexcode[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+    jobject getApplication(JNIEnv *env) {
+        jobject application = NULL;
+        jclass activity_thread_clz = env->FindClass("android/app/ActivityThread");
+        if (activity_thread_clz != NULL) {
+            jmethodID currentApplication = env->GetStaticMethodID(
+                                                                  activity_thread_clz, "currentApplication", "()Landroid/app/Application;");
+            if (currentApplication != NULL) {
+                application = env->CallStaticObjectMethod(activity_thread_clz, currentApplication);
+            } else {
+                LOGE("Cannot find method: currentApplication() in ActivityThread.");
+            }
+            env->DeleteLocalRef(activity_thread_clz);
+        } else {
+            LOGE("Cannot find class: android.app.ActivityThread");
+        }
+        
+        return application;
+    }
+    
+    char* getSha1(JNIEnv *env){
+        //上下文对象
+        jobject application = getApplication(env);
+        if (application == NULL) {
+            return NULL;
+        }
+        jclass context_class = env->GetObjectClass(application);
+        
+        //反射获取PackageManager
+        jmethodID methodId = env->GetMethodID(context_class, "getPackageManager", "()Landroid/content/pm/PackageManager;");
+        jobject package_manager = env->CallObjectMethod(application, methodId);
+        if (package_manager == NULL) {
+            LOGE("package_manager is NULL!!!");
+            return NULL;
+        }
+        
+        //反射获取包名
+        methodId = env->GetMethodID(context_class, "getPackageName", "()Ljava/lang/String;");
+        jstring package_name = (jstring)env->CallObjectMethod(application, methodId);
+        if (package_name == NULL) {
+            LOGE("package_name is NULL!!!");
+            return NULL;
+        }
+        env->DeleteLocalRef(context_class);
+        
+        //获取PackageInfo对象
+        jclass pack_manager_class = env->GetObjectClass(package_manager);
+        methodId = env->GetMethodID(pack_manager_class, "getPackageInfo", "(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;");
+        env->DeleteLocalRef(pack_manager_class);
+        jobject package_info = env->CallObjectMethod(package_manager, methodId, package_name, 0x40);
+        if (package_info == NULL) {
+            LOGE("getPackageInfo() is NULL!!!");
+            return NULL;
+        }
+        env->DeleteLocalRef(package_manager);
+        
+        //获取签名信息
+        jclass package_info_class = env->GetObjectClass(package_info);
+        jfieldID fieldId = env->GetFieldID(package_info_class, "signatures", "[Landroid/content/pm/Signature;");
+        env->DeleteLocalRef(package_info_class);
+        jobjectArray signature_object_array = (jobjectArray)env->GetObjectField(package_info, fieldId);
+        if (signature_object_array == NULL) {
+            LOGE("signature is NULL!!!");
+            return NULL;
+        }
+        jobject signature_object = env->GetObjectArrayElement(signature_object_array, 0);
+        env->DeleteLocalRef(package_info);
+        
+        //签名信息转换成sha1值
+        jclass signature_class = env->GetObjectClass(signature_object);
+        methodId = env->GetMethodID(signature_class, "toByteArray", "()[B");
+        env->DeleteLocalRef(signature_class);
+        jbyteArray signature_byte = (jbyteArray) env->CallObjectMethod(signature_object, methodId);
+        jclass byte_array_input_class=env->FindClass("java/io/ByteArrayInputStream");
+        methodId=env->GetMethodID(byte_array_input_class,"<init>","([B)V");
+        jobject byte_array_input=env->NewObject(byte_array_input_class,methodId,signature_byte);
+        jclass certificate_factory_class=env->FindClass("java/security/cert/CertificateFactory");
+        methodId=env->GetStaticMethodID(certificate_factory_class,"getInstance","(Ljava/lang/String;)Ljava/security/cert/CertificateFactory;");
+        jstring x_509_jstring=env->NewStringUTF("X.509");
+        jobject cert_factory=env->CallStaticObjectMethod(certificate_factory_class,methodId,x_509_jstring);
+        methodId=env->GetMethodID(certificate_factory_class,"generateCertificate",("(Ljava/io/InputStream;)Ljava/security/cert/Certificate;"));
+        jobject x509_cert=env->CallObjectMethod(cert_factory,methodId,byte_array_input);
+        env->DeleteLocalRef(certificate_factory_class);
+        jclass x509_cert_class=env->GetObjectClass(x509_cert);
+        methodId=env->GetMethodID(x509_cert_class,"getEncoded","()[B");
+        jbyteArray cert_byte=(jbyteArray)env->CallObjectMethod(x509_cert,methodId);
+        env->DeleteLocalRef(x509_cert_class);
+        jclass message_digest_class=env->FindClass("java/security/MessageDigest");
+        methodId=env->GetStaticMethodID(message_digest_class,"getInstance","(Ljava/lang/String;)Ljava/security/MessageDigest;");
+        jstring sha1_jstring=env->NewStringUTF("SHA1");
+        jobject sha1_digest=env->CallStaticObjectMethod(message_digest_class,methodId,sha1_jstring);
+        methodId=env->GetMethodID(message_digest_class,"digest","([B)[B");
+        jbyteArray sha1_byte=(jbyteArray)env->CallObjectMethod(sha1_digest,methodId,cert_byte);
+        env->DeleteLocalRef(message_digest_class);
+        
+        //转换成char
+        jsize array_size=env->GetArrayLength(sha1_byte);
+        jbyte* sha1 =env->GetByteArrayElements(sha1_byte,NULL);
+        char *hex_sha=new char[array_size*2+1];
+        for (int i = 0; i <array_size ; ++i) {
+            hex_sha[2*i]=hexcode[((unsigned char)sha1[i])/16];
+            hex_sha[2*i+1]=hexcode[((unsigned char)sha1[i])%16];
+        }
+        hex_sha[array_size*2]='\0';
+        
+        LOGV("hex_sha %s ",hex_sha);
+        return hex_sha;
+    }
+    
+    jboolean checkValidity(JNIEnv *env,char *sha1){
+        //比较签名
+        if (strcmp(sha1,app_sha1)==0)
+        {
+            LOGV("验证成功");
+            return true;
+        }
+        LOGV("验证失败");
+        return false;
+    }
+    
+    jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+        JNIEnv *env = NULL;
+        if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+            return JNI_ERR;
+        }
+        
+        char *sha1 = getSha1(env);
+        jboolean result = checkValidity(env,sha1);
+        if(result){
+            return JNI_VERSION_1_4;
+        }else{
+            return JNI_ERR;
+        }
+    }
 
     jbyteArray Java_io_github_dltech21_Security_EncryptByKey(JNIEnv* env, jobject obj, jstring jstr, jstring jstrKey)
     {
@@ -154,7 +292,7 @@ extern "C" {
         
         WriteUint32((pData + nEncryptLen - 4), nInLen);
         AES_KEY aesKey;
-        const char *key = "00111946655405188575651534545107";
+        const char *key = "42ac40b2e40f06fb1836228fc7c1e587";
         AES_set_encrypt_key((const unsigned char*)key, 256, &aesKey);
         for (uint32_t i = 0; i < nBlocks; i++) {
             AES_encrypt(pData + i * 16, pEncData + i * 16, &aesKey);
@@ -199,7 +337,7 @@ extern "C" {
         uint32_t nBlocks = nLen / 16;
         AES_KEY aesKey;
         
-        const char *key = "00111946655405188575651534545107";
+        const char *key = "42ac40b2e40f06fb1836228fc7c1e587";
         AES_set_decrypt_key((const unsigned char*) key, 256, &aesKey);           //设置AES解密密钥
         for (uint32_t i = 0; i < nBlocks; i++) {
             AES_decrypt(pData + i * 16, (unsigned char*)pTmp + i * 16, &aesKey);
